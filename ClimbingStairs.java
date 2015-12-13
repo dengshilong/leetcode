21fd828ec81fe3a661e5bca28d44366bfd877e41
robinjia <long470884130@163.com>
2015-12-13 19:56:02 +0800

    add ClimbingStairs.java

---
+++ ClimbingStairs.java
@@ -0,0 +1,16 @@
+
+public class ClimbingStairs {
+	public static int climbStairs(int n) {
+		int a = 1;
+		int b = 1;
+		while (n-- > 0) {
+			int temp = a + b;
+			a = b;
+			b = temp;
+		}
+		return a;
+	}
+	public static void main(String[] args) {
+		System.out.println(climbStairs(5));
+	}
+}
