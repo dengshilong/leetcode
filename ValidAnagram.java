486d0fd4463bbe96fe5ea4b5fba7d4871f9667b5
robinjia <long470884130@163.com>
2015-12-14 14:39:51 +0800

    add ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,23 @@
+
+public class ValidAnagram {
+	final static int NUM = 256;
+	public static boolean isAnagram(String s, String t) {
+		int[] count = new int[NUM];
+	    for (int i = 0; i < s.length(); i++) {
+	        count[s.charAt(i)]++;
+	    }
+	    for (int i = 0; i < t.length(); i++) {
+	        count[t.charAt(i)]--;
+	    }
+	    int zeroNum = 0;
+	    for (int i = 0; i < NUM; i++) {
+	        if (count[i] == 0)
+	            zeroNum++;
+	    }
+	    return zeroNum == NUM;
+	}
+	public static void main(String[] args) {
+		System.out.println(isAnagram("anagram", "nagaram"));
+		System.out.println(isAnagram("rat", "car"));
+	}
+}
