4712c1481e37a7ad17c4b2c583682daa6722d7f4
robinjia <long470884130@163.com>
2015-12-14 20:36:37 +0800

    add MinStack.java

---
+++ MinStack.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class MinStack {
+	ArrayList<Integer> element;
+	ArrayList<Integer> min;
+	MinStack() {
+		element = new ArrayList<Integer>();
+		min = new ArrayList<Integer>();
+	}
+	public void push(int x) {
+		element.add(x);
+		if (min.isEmpty()) {
+			min.add(x);
+		} else {
+			min.add(Math.min(x, min.get(min.size() - 1)));
+		}
+    }
+
+    public void pop() {
+    	if (element.isEmpty())
+    		return;
+        element.remove(element.size() - 1);
+        min.remove(min.size() - 1);
+    }
+
+    public int top() {
+        return element.get(element.size() - 1);
+    }
+
+    public int getMin() {
+        return min.get(min.size() - 1);
+    }
+}
