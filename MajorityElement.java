61d989872fea616561e4c2eb6508b8ea980c21cb
robinjia <long470884130@163.com>
2015-12-14 20:00:50 +0800

    MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,24 @@
+
+public class MajorityElement {
+	public static int majorityElement(int[] nums) {
+		int count = 0;
+	    int major = 0;
+	    for (int i = 0; i < nums.length; i++) {
+	        if (count == 0) {
+	        	major = nums[i];
+	            count++;
+	        } else {
+	            if (major != nums[i]) {
+	                count--;
+	            } else {
+	                count++;
+	            }
+	        }
+	    }
+	    return major;
+    }
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 1, 1};
+		System.out.println(majorityElement(nums));
+	}
+}
