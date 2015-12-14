b3618f822408a1c03cfc6ca6b590d6d3a14000be
robinjia <long470884130@163.com>
2015-12-14 15:01:42 +0800

    add PowerOfTwo.java

---
+++ PowerOfTwo.java
@@ -0,0 +1,13 @@
+
+public class PowerOfTwo {
+	public static boolean isPowerOfTwo(int n) {
+		if (n <= 0)
+			return false;
+		return (n & (n - 1)) == 0;
+	}
+	public static void main(String[] args) {
+		for (int i = 1; i < 20; i++) {
+			System.out.println(i + " " + isPowerOfTwo(i));
+		}
+	}
+}
