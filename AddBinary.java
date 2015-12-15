ad1dca56c051e625655ab368c59af8cf735bbc10
robinjia <long470884130@163.com>
2015-12-15 21:01:54 +0800

    add AddBinary.java

---
+++ AddBinary.java
@@ -0,0 +1,36 @@
+
+public class AddBinary {
+	public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int remain = 0;
+        while (i >= 0 && j >= 0) {
+        	int s = (a.charAt(i) - '0') + (b.charAt(j) - '0') + remain;
+        	sb.append(s % 2);
+        	remain = s / 2;
+        	i--;
+        	j--;
+        }
+        while (i >= 0) {
+        	int s = (a.charAt(i) - '0') + remain;
+        	sb.append(s % 2);
+        	remain = s / 2;
+        	i--;
+        }
+        while (i >= 0) {
+        	int s = (b.charAt(j) - '0') + remain;
+        	sb.append(s % 2);
+        	remain = s / 2;
+        	j--;
+        }
+        if (remain > 0) {
+        	sb.append(remain);
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+	public static void main(String[] args) {
+		System.out.println(addBinary("1101", "1111"));
+	}
+}
