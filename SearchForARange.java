7e9c119e24b3c8306800ddd095d9401f891c9876
robinjia <long470884130@163.com>
2015-12-15 18:05:37 +0800

    add SearchForARange.java

---
+++ SearchForARange.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+
+public class SearchForARange {
+	public static int[] searchRange(int[] nums, int target) {
+		int[] res = new int[2];
+        int index = Arrays.binarySearch(nums, target);
+        if (index < 0) {
+        	res[0] = res[1] = -1;
+        } else {
+        	int start = index;
+        	int end = index;
+        	while (start >= 0 && nums[start] == target) {
+        		start--;
+        	}
+        	res[0] = start + 1;
+        	while (end < nums.length && nums[end] == target) {
+        		end++;
+        	}
+        	res[1] = end - 1;
+        }
+    	return res;
+    }
+	public static void main(String[] args) {
+		int[] nums = {5, 7, 7, 8, 8, 10};
+		int[] res = searchRange(nums, 8);
+		for (int i: res) {
+			System.out.println(i + " ");
+		}
+	}
+}
