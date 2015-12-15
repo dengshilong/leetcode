90f7d09199598cd0795f081e1a5d470eed129ee9
robinjia <long470884130@163.com>
2015-12-15 17:31:24 +0800

    add RemoveNthNodeFromEndOfList.java

---
+++ RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,24 @@
+/** 
+* @author: robinjia
+* @emai: dengshilong1988@gmail.com
+* @create：2015年12月15日 下午5:30:01 
+* 
+*/
+public class RemoveNthNodeFromEndOfList {
+	public static ListNode removeNthFromEnd(ListNode head, int n) {
+		 ListNode fast = head;
+		 ListNode slow = head;
+		 while (fast != null) {
+			 if (n-- < 0) {
+				 slow = slow.next;
+			 }
+			 fast = fast.next;
+		 }
+		 if (n < 0) {
+			 slow.next = slow.next.next;
+		 } else {
+			 head = head.next;
+		 }
+		 return head;
+    }
+}
