15a36ef89040178e1dbffd8ca0128bcf53f02733
robinjia <long470884130@163.com>
2015-12-15 22:07:32 +0800

    add Combinations.java

---
+++ Combinations.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Combinations {
+	public static List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if (k == 0 || n == 0) {
+			return result;
+		}
+		combine(1, n, k, result, new ArrayList<Integer>());
+		return result;
+    }
+	private static void combine(int start, int end, int k, List<List<Integer>> result, List<Integer> com) {
+		if (k == 0) {
+			result.add(new ArrayList<Integer>(com));
+			return;
+		}
+		for (int i = start; i <= end; i++) {
+			com.add(i);
+			combine(i + 1, end, k - 1, result, com);
+			com.remove(com.size() - 1);
+		}
+	}
+	public static void main(String[] args) {
+		List<List<Integer>> result = combine(4, 2);
+		for (List<Integer> t: result) {
+			for (Integer i: t) {
+				System.out.print(i + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
