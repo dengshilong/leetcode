51dd948a5219b67dc2bd5066b0baf7e49d850bf9
robinjia <long470884130@163.com>
2015-12-16 20:23:50 +0800

    add LengthOfLastWord.java

---
+++ LengthOfLastWord.java
@@ -0,0 +1,8 @@
+
+public class LengthOfLastWord {
+	public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] strs = s.split(" ");
+        return strs[strs.length - 1].length();
+    }
+}
