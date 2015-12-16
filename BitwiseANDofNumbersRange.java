9c836679f9c00d654877e6db2361b2781c9ed92c
robinjia <long470884130@163.com>
2015-12-16 14:37:55 +0800

    add BitwiseANDofNumbersRange.java

---
+++ BitwiseANDofNumbersRange.java
@@ -0,0 +1,15 @@
+
+public class BitwiseANDofNumbersRange {
+	public static int rangeBitwiseAnd(int m, int n) {
+        int i = 0;
+        while (m != n) {
+        	m >>= 1;
+        	n >>= 1;
+        	i++;
+        }
+        return n << i;
+    }
+	public static void main(String[] args) {
+		System.out.println(rangeBitwiseAnd(4, 7));
+	}
+}
