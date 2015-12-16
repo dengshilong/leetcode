ac45fed59aea59e5b04377b2c80fa7b8d5e2fe50
robinjia <long470884130@163.com>
2015-12-16 11:33:46 +0800

    add MinimumDepthOfBinaryTree.java

---
+++ MinimumDepthOfBinaryTree.java
@@ -0,0 +1,11 @@
+/** 
+* @author: robinjia
+* @emai: dengshilong1988@gmail.com
+* @create：2015年12月16日 上午11:31:46 
+* 
+*/
+public class MinimumDepthOfBinaryTree {
+	public int minDepth(TreeNode root) {
+	        
+	}
+}
