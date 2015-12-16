d085cac94858aa3f2f786f4fb71f6c1a6795fecc
robinjia <long470884130@163.com>
2015-12-16 10:43:26 +0800

    add ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,42 @@
+
+
+public class ReverseLinkedList {
+	public static ListNode reverseList(ListNode head) {
+		if (head == null || head.next == null)
+			return head;
+		ListNode cur = head;
+		ListNode next = cur.next;
+		cur.next = null;
+		while (next != null) {
+			ListNode temp = next.next;
+			next.next = cur;
+			cur = next;
+			next = temp;
+		}
+		return cur;
+    }
+	public static ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode first = head;
+        ListNode second = head;
+        while (n > 1) {
+        	first = first.next;
+        	n--;
+        }
+        ListNode pre = null;
+        while (m > 1) {
+        	pre = second;
+        	second = second.next;
+        	m--;
+        }
+        ListNode other = first.next;
+        first.next = null;
+        ListNode temp = reverseList(second);
+        second.next = other;
+        if (pre != null) {
+        	pre.next = temp;
+        	return head;
+        } else {
+        	return temp;
+        }
+    }
+}
