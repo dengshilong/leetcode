35001d06029ef346dcf97867a521db9d11e754df
robinjia <long470884130@163.com>
2015-12-16 11:06:55 +0800

    add MaximumDepthOfBinaryTree.java

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,20 @@
+/** 
+* @author: robinjia
+* @emai: dengshilong1988@gmail.com
+* @create：2015年12月16日 上午11:05:09 
+* 
+*/
+public class MaximumDepthOfBinaryTree {
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+	        return 0;
+	    }
+	    int maxLeftDepth = 0;
+	    if (root.left != null)
+	        maxLeftDepth = maxDepth(root.left);
+	    int maxRightDepth = 0;
+	    if (root.right != null)
+	        maxRightDepth = maxDepth(root.right);
+	    return maxLeftDepth >= maxRightDepth ? 1 + maxLeftDepth : 1 + maxRightDepth;
+    }
+}
