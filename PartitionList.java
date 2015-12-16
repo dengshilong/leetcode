a891b74b45b6a5a0a4dd0d63f0e1a887e31086a0
robinjia <long470884130@163.com>
2015-12-16 10:31:03 +0800

    add PartitionList.java

---
+++ PartitionList.java
@@ -0,0 +1,26 @@
+/** 
+* @author: robinjia
+* @emai: dengshilong1988@gmail.com
+* @create：2015年12月16日 上午10:30:34 
+* 
+*/
+public class PartitionList {
+	public ListNode partition(ListNode head, int x) {
+		if (head == null || head.next == null)
+			return head;
+		ListNode cur = head;
+		ListNode next = partition(head.next, x);
+		cur.next = next;
+		if (cur.val >= x) {
+			if (next != null && next.val < x) {
+				head = next;
+				while (next.next != null && next.next.val < x) {
+					next = next.next;
+				}
+				cur.next = next.next;
+				next.next = cur;
+			}
+		}
+		return head;
+	}
+}
