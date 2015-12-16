1cba5075d14d11fd420c37933ed74a1a275dba22
robinjia <long470884130@163.com>
2015-12-16 10:59:11 +0800

    add SymmertricTree.java

---
+++ SymmetricTree.java
@@ -0,0 +1,31 @@
+/** 
+* @author: robinjia
+* @emai: dengshilong1988@gmail.com
+* @create：2015年12月16日 上午10:58:23 
+* 
+*/
+public class SymmetricTree {
+	public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+        	return true;
+        } else {
+        	return isSymmetricHelper(root.left, root.right);
+        }
+    }
+	public static boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+		if (left == null && right == null) {
+			return true;
+		} else if (left == null && right != null) {
+			return false;
+		} else if (left != null && right == null) {
+			return false;
+		} else {
+			if (left.val != right.val){
+				return false;
+			} else {
+				return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+		
+			}
+		}
+	}
+}
