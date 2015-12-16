801cee138bcf7655d611ef326fa6ea260dff555d
robinjia <long470884130@163.com>
2015-12-16 21:31:38 +0800

    add IsomorphicStrings.java

---
+++ IsomorphicStrings.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class IsomorphicStrings {
+	public static boolean isIsomorphic(String s, String t) {
+		Map<String, String> map = new HashMap<String, String>();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			if (!map.containsKey(String.valueOf(s.charAt(i)))) {
+				if (map.containsValue(String.valueOf(t.charAt(i))))
+					return false;
+				map.put(String.valueOf(s.charAt(i)), String.valueOf(t.charAt(i)));
+			}
+			sb.append(map.get(String.valueOf(s.charAt(i))));
+		}
+		String res = sb.toString();
+		//System.out.println(res);
+		return res.equals(t);
+    }
+	public static void main(String[] args) {
+		System.out.println(isIsomorphic("egg", "abb"));
+		System.out.println(isIsomorphic("egg", "abc"));
+		System.out.println(isIsomorphic("ab", "aa"));
+	}
+}
