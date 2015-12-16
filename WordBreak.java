ef91bd9644a463c6de719b02459a2f8e0c8666e5
robinjia <long470884130@163.com>
2015-12-16 12:09:15 +0800

    add WordBreak.java

---
+++ WordBreak.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+
+
+public class WordBreak {
+	public boolean wordBreak(String s, Set<String> wordDict) {
+		boolean[] dp = new boolean[s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			if (!dp[i] && wordDict.contains(s.substring(0, i + 1))) {
+				dp[i] = true;
+			}
+			if (dp[i]) {
+				for (int j = i + 1; j < s.length(); j++) {
+					if (wordDict.contains(s.substring(i + 1, j + 1))) {
+						dp[j] = true;
+					}
+				}
+			}
+		}
+		return dp[s.length() - 1];
+    }
+	
+	public static void main(String[] args) {
+		String s = "abcde";
+		for (int i = 0; i < s.length(); i++) {
+			System.out.println(s.substring(0, i + 1));
+		}
+	}
+}
