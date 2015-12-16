e577bf2775540444691ad97ac4a5016887786fa0
robinjia <long470884130@163.com>
2015-12-16 11:30:46 +0800

    add BalancedBinaryTree.java

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,34 @@
+/** 
+* @author: robinjia
+* @emai: dengshilong1988@gmail.com
+* @create：2015年12月16日 上午11:26:00 
+* 
+*/
+public class BalancedBinaryTree {
+	public boolean isBalanced(TreeNode root) {
+		if (root == null) {
+	        return true;
+	    }
+	    int maxLeftDepth = maxDepth(root.left);
+	    int maxRightDepth = maxDepth(root.right);
+	    if (Math.abs(maxLeftDepth - maxRightDepth) > 1)
+	        return false;
+	    if (root.left != null && !isBalanced(root.left))
+	        return false;
+	    if (root.right != null && !isBalanced(root.right))
+	        return false;
+	    return true;
+    }
+	public static int maxDepth(TreeNode root) {
+		if (root == null) {
+	        return 0;
+	    }
+	    int maxLeftDepth = 0;
+	    if (root.left != null)
+	        maxLeftDepth = maxDepth(root.left);
+	    int maxRightDepth = 0;
+	    if (root.right != null)
+	        maxRightDepth = maxDepth(root.right);
+	    return maxLeftDepth >= maxRightDepth ? 1 + maxLeftDepth : 1 + maxRightDepth;
+    }
+}
