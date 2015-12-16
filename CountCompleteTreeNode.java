ddff59a3baa0dfff88b63b1b0ff115175b62fc6e
robinjia <long470884130@163.com>
2015-12-16 20:13:44 +0800

    add CountCompleteTreeNode.java

---
+++ CountCompleteTreeNode.java
@@ -0,0 +1,20 @@
+
+
+
+public class CountCompleteTreeNode {
+	public int countNodes(TreeNode root) {  
+		if (root == null) {
+			return 0;
+		}
+        int leftDepth = 0;
+        int rightDepth = 0;
+        for (TreeNode cur = root.left; cur != null; cur = cur.left)  leftDepth++;
+        for (TreeNode cur = root.right; cur != null; cur = cur.right)  rightDepth++;
+        if (leftDepth == rightDepth) {
+        	return 2 << leftDepth - 1;
+        } 
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+	
+	
+}
