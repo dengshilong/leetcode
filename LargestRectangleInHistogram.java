e26d08d8cbf116fcb79fbd6d7539774bc4884106
robinjia <long470884130@163.com>
2015-12-16 10:28:46 +0800

    add LargestRectangleInhistogram.java

---
+++ LargestRectangleInHistogram.java
@@ -0,0 +1,36 @@
+
+public class LargestRectangleInHistogram {
+	public static int largestRectangleArea(int[] height) {
+		/* use monotonous stack */
+		if (height.length == 0)
+			return 0;
+        int[] stack = new int[height.length + 1];
+        int top = 0;
+        stack[top] = -1;
+        int result = 0;
+        for (int i = 0; i <= height.length; i++) {
+        	int h;
+        	if (i == height.length) {
+        		h = -1;
+        	} else {
+        		h = height[i];
+        	}
+        	while (top != 0 && height[stack[top]] > h) {
+        		int temp = height[stack[top]] * (i - 1 - stack[top - 1]);
+        		if (temp > result) {
+        			result = temp;
+        		}
+        		top--;
+        	}
+        	top++;
+        	stack[top] = i;
+        }
+        return result;
+    }
+	public static void main(String[] args) {
+		int[] height = {2,1,5,6,2,3};
+		System.out.println(largestRectangleArea(height));
+		System.out.println(largestRectangleArea(new int[]{}));
+		System.out.println(largestRectangleArea(new int[]{0}));
+	}
+}
