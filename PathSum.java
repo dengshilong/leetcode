30a819078b946524b0265063b246d54e5e0f9f90
robinjia <long470884130@163.com>
2015-12-16 11:35:38 +0800

    add PathSum.java

---
+++ PathSum.java
@@ -0,0 +1,11 @@
+/** 
+* @author: robinjia
+* @emai: dengshilong1988@gmail.com
+* @create：2015年12月16日 上午11:34:28 
+* 
+*/
+public class PathSum {
+	public boolean hasPathSum(TreeNode root, int sum) {
+        
+    }
+}
