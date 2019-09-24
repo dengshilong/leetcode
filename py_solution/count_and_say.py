760c19fdbe9b48c43eaec23013aec5c73c4c50e5
dengshilong <dengshilong1988@gmail.com>
2019-09-24 22:14:35 +0800

    fix: 修改报数

--- py_solution/count_and_say.py
+++ py_solution/count_and_say.py
@@ -7,19 +7,15 @@ class Solution(object):
         res = '1'
         while n > 1:
             temp = []
-            count = 1
-            cur = res[0]
-            for i in range(1, len(res)):
-                if res[i] == cur:
+            i = 0
+            while i < len(res):
+                count = 1
+                cur = res[i]
+                while i + count < len(res) and res[i] == res[i + count]:
                     count += 1
-                else:
-                    temp.append(str(count))
-                    temp.append(cur)
-                    cur = res[i]
-                    count = 1
-            if count:
                 temp.append(str(count))
                 temp.append(cur)
+                i += count
             res = ''.join(temp)
             n -= 1
         return res
@@ -27,5 +23,5 @@ class Solution(object):
 
 if __name__ == "__main__":
     solution = Solution()
-    # assert solution.countAndSay(1) == '1'
+    assert solution.countAndSay(1) == '1'
     assert solution.countAndSay(4) == '1211'
