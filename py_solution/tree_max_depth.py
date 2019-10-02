60850b736e4e903a37b693dfd3e03de2ef89bf6f
dengshilong <dengshilong1988@gmail.com>
2019-10-02 16:11:11 +0800

    docs: 最大深度

---
+++ py_solution/tree_max_depth.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+if __name__ == "__main__":
+    pass
