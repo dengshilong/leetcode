4b51ebc05057d5b6bb8a03a8871876858921e7ce
dengshilong <dengshilong1988@gmail.com>
2019-10-02 10:03:16 +0800

    docs: 增加1的次数

---
+++ py_solution/count_one.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n > 0:
+            n = n & n - 1
+            count += 1
+        return count
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    assert solution.hammingWeight(11) == 3
